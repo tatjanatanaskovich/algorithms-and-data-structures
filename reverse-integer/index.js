ad0b10ae623ea8f5426f036b4a6c3bdd456e7da4
tatjanatanaskovich <tatjanatanaskovich@gmail.com>
2019-07-25 08:48:15 +0200

    Reverse an Integer

---
+++ reverse-integer/index.js
@@ -0,0 +1,18 @@
+// Reverse an integer
+//Given an integer, return an integer that is the reverse ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(number) {
+  const reversed = number
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+  return parseInt(reversed) * Math.sign(number);
+}
